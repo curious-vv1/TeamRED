e99173eebe8652ba9611725e84f73e8e940da69b
curious-vv1 <vivekrealc2@gmail.com>
2024-03-05 03:40:15 +0530

    beginning

--- src/App.js
+++ src/App.js
@@ -2,9 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Projects from './components/Projects';
+
 
 export default function App() {
+
+
+ const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Home />
+  },
+  {
+    path:"/projects",
+    element:<Projects />
+  }
+ ])
+
+
   return (
-    <Home />
+    <RouterProvider router={router} />
   )
 }
